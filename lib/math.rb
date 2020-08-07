529763869f3e902f8db36d0001cac1682420bfb0
Aimee Liang <aimeeeliang@gmail.com>
2020-08-07 01:30:52 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,25 +5,25 @@ end
 
 def subtraction(num1, num2)
   subtract_total = num1 + num2
-  puts subtract_total
+  return subtract_total
 end
 
 def division(num1, num2)
   divided_total = num1 / num2
-  puts divided_total
+  return divided_total
 end
 
 def multiplication(num1, num2)
   multiply_total = num1 * num2
-  puts multiply_total
+  return multiply_total
 end
 
 def modulo(num1, num2)
   modulo_total = num1 % num2
-  puts modulo_total
+  return modulo_total
 end
 
 def square_root(num)
   sqrt_total = Math.sqrt(num)
-  puts sqrt_total
+  return sqrt_total
 end
