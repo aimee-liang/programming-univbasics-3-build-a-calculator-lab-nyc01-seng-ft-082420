fec4b8502a56bdb94dc35e3ea02ed298ece2b85d
Aimee Liang <aimeeeliang@gmail.com>
2020-08-07 01:20:04 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+  add_total = num1 + num2
+  puts add_total
 end
 
 def subtraction(num1, num2)
-
+  subtract_total = num1 + num2
+  puts subtract_total
 end
 
 def division(num1, num2)
-
+  divided_total = num1 / num2
+  puts divided_total
 end
 
 def multiplication(num1, num2)
-
+  multiply_total = num1 * num2
+  puts multiply_total
 end
 
 def modulo(num1, num2)
